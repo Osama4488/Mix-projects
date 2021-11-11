d15a14c7b41bd4355ff89524fc55bc7ecf988ade
Osama4488 <syed.osama.ali.96@gmail.com>
2021-11-12 01:56:24 +0500

    'pushing'

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -3,13 +3,13 @@ import { db } from "../../../firebase.js";
 import { collection, addDoc } from "firebase/firestore";
 
 export default function Signin() {
-  const docRef = doc(db, "users");
-  const docSnap = getDoc(docRef);
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-  } else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-  }
+  // const docRef = doc(db, "users");
+  // const docSnap = getDoc(docRef);
+  // if (docSnap.exists()) {
+  //   console.log("Document data:", docSnap.data());
+  // } else {
+
+  //   console.log("No such document!");
+  // }
   return <></>;
 }
