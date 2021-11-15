86a7e0390408aba9342c87d55ce3cfc88f6fab78
Osama4488 <syed.osama.ali.96@gmail.com>
2021-11-16 01:31:17 +0500

    'pushing'

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,15 +1,98 @@
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../../firebase.js";
 import { collection, addDoc } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
+import { ref, onValue } from "firebase/database";
+import { useState, useEffect } from "react";
 
 export default function Signin() {
-  // const docRef = doc(db, "users");
-  // const docSnap = getDoc(docRef);
-  // if (docSnap.exists()) {
-  //   console.log("Document data:", docSnap.data());
-  // } else {
+  const [users, setUsers] = useState([
+    {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+    },
+  ]);
 
-  //   console.log("No such document!");
-  // }
-  return <></>;
+  useEffect(() => {
+    const db = getDatabase();
+    const starCountRef = ref(db, "users");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      // setUsers(data);
+      // console.log(data, "data");
+      Object.keys(data).forEach((key) => {
+        // The ID is the key
+        console.log(key, "id");
+        // console.log(data[key], "id data");
+        const newData = data[key];
+        const { firstname, lastname, email, password } = data;
+        // setUsers(data[key]);
+        setUsers([...users, { newData }]);
+        console.log(users, "state data");
+        console.log(newData, "new data");
+      });
+      // console.log(users, "state data");
+      // updateStarCount(postElement, data);
+    });
+    console.log(starCountRef, "a");
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="container-left">
+          <div className="wrapper-left">
+            <div className="part-one">
+              <h3>Get started</h3>
+              <p>Lets create your account</p>
+            </div>
+            <div className="inp">
+              <form action="">
+                <label for="" className="email">
+                  E-mail{" "}
+                </label>
+                <i className="fas fa-envelope envelope"></i>
+                <input
+                  className="inp-left"
+                  type="email"
+                  placeholder="mhmmd.rezaei4@gmail.com"
+                />
+                <label for="" className="pass">
+                  Password{" "}
+                </label>
+                <i className="fas fa-lock pass"></i>
+                <input
+                  className="inp-left"
+                  type="password"
+                  placeholder="8+ characters, 2 numbers"
+                  name=""
+                  id=""
+                />
+              </form>
+            </div>
+            <div className="buttons">
+              <button className="btn-one">Create an account</button>
+              <button className="btn-two">
+                <i className="fab fa-google"></i> Sign up with google
+              </button>
+            </div>
+            <div className="check">
+              <label for="privacy">
+                I read and accept the User Agreement and <br />
+                Privacy Policy.
+                <input id="privacy" type="checkbox" />
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <div className="down">
+              <p>Already have an account?</p>
+              <a href="#"> Log in </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,15 +3,19 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { db } from "../../../firebase.js";
 import { collection, addDoc } from "firebase/firestore";
-
+import { Spin, Alert } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 export default function Signup() {
   const [userData, setUserData] = useState({
     firstname: "",
     lastname: "",
     email: "",
     password: "",
-    address: "",
+    loading: false,
+    error: "",
+    posted: "",
   });
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
   useEffect(() => {
     // getFirebase();
   }, []);
@@ -55,9 +59,10 @@ export default function Signup() {
   //   }
   // };
   const onSubmitFirebase = async (event) => {
+    setUserData({ ...userData, loading: true });
     event.preventDefault();
-    const { firstname, lastname, email, password, address } = userData;
-    if (firstname && lastname && email && password && address) {
+    const { firstname, lastname, email, password } = userData;
+    if (firstname && lastname && email && password) {
       const res = await fetch(
         "https://osamaflix-17d99-default-rtdb.firebaseio.com/users.json",
         {
@@ -70,7 +75,6 @@ export default function Signup() {
             lastname,
             email,
             password,
-            address,
           }),
         }
       );
@@ -80,19 +84,134 @@ export default function Signup() {
           lastname: "",
           email: "",
           password: "",
-          address: "",
+          loading: false,
+          posted: "true",
         });
-        alert("Data stored");
       } else {
-        alert("error");
+        setUserData({ ...userData, error: "true", posted: "false" });
       }
     } else {
-      alert("plzz fill the data");
+      setUserData({ ...userData, error: "empty", posted: "false" });
     }
   };
   return (
     <>
       <div className="container">
+        <div className="container-left">
+          <div className="wrapper-left">
+            <div className="part-one">
+              <h3>Get started</h3>
+              <p>Lets create your account</p>
+            </div>
+
+            {userData.posted === "true" && (
+              <Alert
+                description="User has sucessfully been Registered"
+                type="success"
+                showIcon
+              />
+            )}
+            {userData.error === "true" ? (
+              <Alert
+                message="Error"
+                description="This is an error while doing signup please try again"
+                type="error"
+                showIcon
+              />
+            ) : userData.error === "empty" ? (
+              <Alert
+                message="Error"
+                description="Please fill all fields"
+                type="error"
+                showIcon
+              />
+            ) : (
+              <> </>
+            )}
+            <div className="inp">
+              <form action="" onSubmit={(e) => onSubmit(e)}>
+                <label for="" className="email">
+                  First Name
+                </label>
+                <i className="fas fa-envelope envelope"></i>
+                <input
+                  className="inp-left"
+                  placeholder="mhmmd.rezaei4@gmail.com"
+                  value={userData.firstname}
+                  onChange={postUserData}
+                  name="firstname"
+                  id="firstname"
+                  required
+                />
+
+                <label for="" className="pass">
+                  Last Name
+                </label>
+                <i className="fas fa-lock pass"></i>
+                <input
+                  className="inp-left"
+                  placeholder="Last Name"
+                  value={userData.lastname}
+                  onChange={postUserData}
+                  name="lastname"
+                  id="lastname"
+                  required
+                />
+                <label for="" className="pass">
+                  Email
+                </label>
+                <i className="fas fa-lock pass"></i>
+                <input
+                  className="inp-left"
+                  value={userData.email}
+                  onChange={postUserData}
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                />
+                <label for="" className="pass">
+                  Password
+                </label>
+                <i className="fas fa-lock pass"></i>
+                <input
+                  className="inp-left"
+                  value={userData.password}
+                  onChange={postUserData}
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
+                />
+              </form>
+            </div>
+            <div className="buttons">
+              <button onClick={onSubmitFirebase} className="btn-one">
+                Create an account{" "}
+                {userData.loading ? <Spin indicator={antIcon} /> : ""}
+              </button>
+              <button className="btn-two">
+                <i className="fab fa-google"></i> Sign up with google
+              </button>
+            </div>
+            <div className="check">
+              <label for="privacy">
+                I read and accept the User Agreement and <br />
+                Privacy Policy.
+                <input id="privacy" type="checkbox" />
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <div className="down">
+              <p>Already have an account?</p>
+              <a href="#"> Log in </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* end */}
+      {/* <div className="container">
         <form className="form-1" onSubmit={(e) => onSubmit(e)}>
           <h1>Signup</h1>
           <label htmlFor="First Name">First Name</label>
@@ -135,20 +254,9 @@ export default function Signup() {
             Login
           </button>
 
-          {/* <p>Or SignUp Using</p>
-        <div className="icons">
-          <a href="https://www.facebook.com/" target="blank"
-            ><i className="fab fa-facebook-f"></i
-          ></a>
-          <a href="https://twitter.com/" target="blank"
-            ><i className="fab fa-twitter"></i
-          ></a>
-          <a href="https://mail.google.com/" target="blank"
-            ><i className="fab fa-google"></i
-          ></a>
-        </div> */}
+         
         </form>
-      </div>
+      </div> */}
     </>
   );
 }
